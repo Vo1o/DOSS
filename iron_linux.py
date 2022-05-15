bbd957c8a28ad19670905f43ad6cf1cbeddddf55
volodya <92124716+Vo1o@users.noreply.github.com>
2022-05-16 01:04:24 +0300

    Update iron_linux.py

--- iron_linux.py
+++ iron_linux.py
@@ -9,7 +9,7 @@ G = '\033[32m'  # green
 C = '\033[36m'  # cyan
 W = '\033[0m'   # white
 Y = '\033[33m'  # yellow
-
+import psutil
 partitions = psutil.disk_partitions()
 for partition in partitions:
     try:
